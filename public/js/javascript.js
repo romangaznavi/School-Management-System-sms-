d83e94173b28c07361395163c13e33f214db7b65
Wida Bahar <widabahar4@gmail.com>
2019-06-13 15:52:02 +0430

    Enhance layout in student list

---
+++ public/js/javascript.js
@@ -0,0 +1,10 @@
+
+// SideNav Button Initialization
+2
+$(".button-collapse").sideNav();
+3
+// SideNav Scrollbar Initialization
+4
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+5
+var ps = new PerfectScrollbar(sideNavScrollbar);
